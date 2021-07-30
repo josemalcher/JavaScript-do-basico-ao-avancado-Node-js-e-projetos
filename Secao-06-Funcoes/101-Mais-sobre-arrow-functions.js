267875e43e63546da5998987c8e2135b1a19100c
José Malcher Jr <contato@josemalcher.net>
2021-07-30 19:00:01 -0300

    101. Mais sobre arrow functions

---
+++ Secao-06-Funcoes/101-Mais-sobre-arrow-functions.js
@@ -0,0 +1,9 @@
+const multiplicarPorDois = (x) => {
+    return x * 2;
+}
+
+console.log(multiplicarPorDois(2)); // 4
+
+const multiplicarPorDois2 = x => x * 2;
+
+console.log(multiplicarPorDois2(4)); // 8
